cb23ec3a4832671e910cae624c71d8bdb4b347c4
Yeji-Yoon <angelayoon0102@gmail.com>
2025-01-30 23:25:35 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+
+function superbowlWin(record){
+    let foundItem = record.find((object)=>object["result"]==="W")
+    if (foundItem===undefined){
+        return foundItem
+    }
+    return foundItem["year"]
+}
